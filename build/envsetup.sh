3407ef9cad2eeab32aef17b5646844e943b57473
wangbo <njitwb@hotmail.com>
2023-03-06 17:14:57 +0800

    tmp save
    Signed-off-by: wangbo <njitwb@hotmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -3,7 +3,8 @@ export BUILD_DIR=${TOP_DIR}/out
 export GL_UI=false
 export CONFIG_FILE=${TOP_DIR}/devices/mt2500/setup_config.yml
 export PROFILES_DIR=${TOP_DIR}/devices/mt2500/profiles
-export PROFILES="target_mt7981_gl-mt2500 luci custom"
+export PROFILES="target_mt7981_gl-mt2500 luci custom glinet_depends glinet_nas"
 export GOPROXY=https://proxy.golang.com.cn,direct
+export GL_PKGDIR=/data/work/build-gl.inet-MT2500/out/gl_sdk/mt7981
 
 echo "include build/main.mk" > ${TOP_DIR}/Makefile
